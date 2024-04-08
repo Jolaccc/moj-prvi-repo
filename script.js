8392e4f864a3596368fb092700e180bb503efb85
Jolaccc <jopip999@gmail.com>
2024-04-08 12:05:22 +0200

    new console.log

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let hello = "Moj prvi repozitorij";
 let bok = "Promjena";
 let promjena = "Promjena druga";
+let change = "Promjena treca";
 
 console.log(hello);
 console.log(bok);
 console.log(promjena);
+console.log(change);
